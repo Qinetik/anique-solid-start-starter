c1a0b9103775d4c829a9dd4a9a084399d4968465
Waqas Tahir <wakaztahir@gmail.com>
2023-10-01 15:26:55 +0500

    tested with latest versions

--- src/root.tsx
+++ src/root.tsx
@@ -17,7 +17,7 @@ export default function Root() {
     return (
         <Html lang="en" class={"dark"}>
             <Head>
-                <Title>SolidStart - With TailwindCSS</Title>
+                <Title>SolidStart - With Anique</Title>
                 <Meta charset="utf-8"/>
                 <Meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <AniqueThemeLight containerCssSelector={".light"}/>
