6e27384d6c1b9e33b3763173adfc7eaed4814763
Waqas Tahir <wakaztahir@gmail.com>
2023-10-29 10:47:44 +0500

    working with new versions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
-import {A} from 'solid-start'
-import {Headline} from "@qinetik/anique";
+import {Headline, Scaffold} from "@qinetik/anique";
 import StyledCounter from "~/components/StyledCounter";
+import Navigation from "~/components/Navigation";
+import WebAppBar from "~/components/AppBar";
 
 export default function Home() {
     return (
         <main>
-            <Headline>Hello world!</Headline>
-            <h1>Here's a styled counter</h1>
-            <StyledCounter/>
+            <Scaffold
+                drawerContent={() => <Navigation/>}
+                topBar={() => <WebAppBar/>}
+            >
+                <Headline>Hello world!</Headline>
+                <h1>Here's a styled counter</h1>
+                <StyledCounter/>
+            </Scaffold>
         </main>
     )
 }
